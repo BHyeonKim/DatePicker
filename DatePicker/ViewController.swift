e853e331faeff52307a0c0c8de48d421278d4e37
BoHyunK <rlaqhguse@gmail.com>
2020-07-31 14:43:50 +0900

    timer

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -9,13 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let timeSelecter: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
     @IBOutlet var lblCurrentTime: UILabel!
     @IBOutlet var lblPickerTime: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelecter, userInfo: nil, repeats:  true)
     }
+    
+    @objc func updateTime(){
+        lblCurrentTime.text = String(count)
+        count = count + 1
+    }
+    
     @IBAction func changeDatePicker(_ sender: UIDatePicker) {
         let datePickerView = sender
         
